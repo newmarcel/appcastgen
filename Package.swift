17364411b996d29bdfddd228e813ca6663e06b79
Marcel Dierkes <marcel@dierkes.co>
2023-10-03 09:46:21 +0200

    Used an Ink fork to fix Markdown list parsing

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
-        .package(url: "https://github.com/johnsundell/ink.git", from: "0.5.0")
+        .package(url: "https://github.com/mflint/Ink.git", branch: "multiple-depth-list-parsing")
     ],
     targets: [
         .executableTarget(
